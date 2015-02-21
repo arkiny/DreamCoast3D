4c882e7ed410e434d2317acb3c2d027ff93f2e82
Heedong Lee <arkinylee@gmail.com>
2015-02-21 14:33:34 +0900

    022015 컴파일할때 재체크 할것
    HD: 쏘리

--- DreamCoast3D/DreamCoast3D/cGameMapHeightGridSystem.h
+++ DreamCoast3D/DreamCoast3D/cGameMapHeightGridSystem.h
@@ -13,8 +13,8 @@ public:
 	virtual 	~cGameMapHeightGridSystem();
 
 	virtual void Setup();
-	virtual void Update(float fDelta);
-	virtual void Render();
+	virtual void Update(float fDelta) {}
+	virtual void Render() {}
 
 	virtual void AddObjToGrid();
 	virtual void RemoveObjFromGrid();
