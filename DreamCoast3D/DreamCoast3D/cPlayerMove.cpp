665dcd844bfb68545c64ba718c6251d121d179e3
Heedong Lee <arkinylee@gmail.com>
2015-02-24 13:10:32 +0900

    022415 Fix PlayerMove

--- DreamCoast3D/DreamCoast3D/cPlayerMove.cpp
+++ DreamCoast3D/DreamCoast3D/cPlayerMove.cpp
@@ -17,46 +17,52 @@ void cPlayerMove::Start(cGamePlayableObject* pPlayer){
 
 void cPlayerMove::Execute(cGamePlayableObject* pPlayer, float fDelta){
 	D3DXVECTOR3 newPos;
-	if (g_pControlManager->GetInputInfo('W')){
-		D3DXVECTOR3 curPos = pPlayer->GetPosition();
-		D3DXVECTOR3 addVec = (pPlayer->GetFront()*fDelta*pPlayer->GetMoveSpeed());
-		newPos = curPos + addVec;
-		if (pPlayer->GetGridTileSystem()->GetAdjObject(newPos.x, newPos.z).size() == 0){
-			pPlayer->SetPosition(newPos);
+	if (g_pControlManager->GetInputInfo('W') ||
+		g_pControlManager->GetInputInfo('A') ||
+		g_pControlManager->GetInputInfo('S') ||
+		g_pControlManager->GetInputInfo('D')){
+		if (g_pControlManager->GetInputInfo('W')){
+			D3DXVECTOR3 curPos = pPlayer->GetPosition();
+			D3DXVECTOR3 addVec = (pPlayer->GetFront()*fDelta*pPlayer->GetMoveSpeed());
+			newPos = curPos + addVec;
+			if (pPlayer->GetGridTileSystem()->GetAdjObject(newPos.x, newPos.z).size() == 0){
+				pPlayer->SetPosition(newPos);
+			}
 		}
-	}
 
-	if (g_pControlManager->GetInputInfo('S')){
-		D3DXVECTOR3 curPos = pPlayer->GetPosition();
-		D3DXVECTOR3 addVec = (pPlayer->GetFront()*fDelta*pPlayer->GetMoveSpeed());
-		newPos = curPos - addVec;
-		if (pPlayer->GetGridTileSystem()->GetAdjObject(newPos.x, newPos.z).size() == 0){
-			pPlayer->SetPosition(newPos);
+		if (g_pControlManager->GetInputInfo('S')){
+			D3DXVECTOR3 curPos = pPlayer->GetPosition();
+			D3DXVECTOR3 addVec = (pPlayer->GetFront()*fDelta*pPlayer->GetMoveSpeed());
+			newPos = curPos - addVec;
+			if (pPlayer->GetGridTileSystem()->GetAdjObject(newPos.x, newPos.z).size() == 0){
+				pPlayer->SetPosition(newPos);
+			}
 		}
-	}
 
-	if (g_pControlManager->GetInputInfo('A')){
-		D3DXMATRIXA16 matR;
-		float angle = pPlayer->GetPlayerAngle();
-		angle -= fDelta* 4.0f;
-		pPlayer->SetPlayerAngle(angle);
-		D3DXMatrixRotationY(&matR, angle);
-		D3DXVECTOR3 vDir = D3DXVECTOR3(0, 0, 1.f);
-		D3DXVec3TransformNormal(&vDir, &vDir, &matR);
-		pPlayer->SetFront(vDir);
-		pPlayer->SetYangle(angle);
-	}
+		if (g_pControlManager->GetInputInfo('A')){
+			D3DXMATRIXA16 matR;
+			float angle = pPlayer->GetPlayerAngle();
+			angle -= fDelta* 4.0f;
+			pPlayer->SetPlayerAngle(angle);
+			D3DXMatrixRotationY(&matR, angle);
+			D3DXVECTOR3 vDir = D3DXVECTOR3(0, 0, 1.f);
+			D3DXVec3TransformNormal(&vDir, &vDir, &matR);
+			pPlayer->SetFront(vDir);
+			pPlayer->SetYangle(angle);
+		}
 
-	if (g_pControlManager->GetInputInfo('D')){
-		D3DXMATRIXA16 matR;
-		float angle = pPlayer->GetPlayerAngle();
-		angle += fDelta* 4.0f;
-		pPlayer->SetPlayerAngle(angle);
-		D3DXMatrixRotationY(&matR, angle);
-		D3DXVECTOR3 vDir = D3DXVECTOR3(0, 0, 1.f);
-		D3DXVec3TransformNormal(&vDir, &vDir, &matR);
-		pPlayer->SetFront(vDir);
-		pPlayer->SetYangle(angle);
+		if (g_pControlManager->GetInputInfo('D')){
+			D3DXMATRIXA16 matR;
+			float angle = pPlayer->GetPlayerAngle();
+			angle += fDelta* 4.0f;
+			pPlayer->SetPlayerAngle(angle);
+			D3DXMatrixRotationY(&matR, angle);
+			D3DXVECTOR3 vDir = D3DXVECTOR3(0, 0, 1.f);
+			D3DXVec3TransformNormal(&vDir, &vDir, &matR);
+			pPlayer->SetFront(vDir);
+			pPlayer->SetYangle(angle);
+		}
+		return;
 	}
 
 	pPlayer->ChangeState(pPlayer->EPLAYABLESTATE_IDLE);
