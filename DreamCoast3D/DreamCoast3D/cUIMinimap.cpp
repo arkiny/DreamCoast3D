14e6a347011baf0ee3e6b53e3b7e3a15b33afcde
Heedong Lee <arkinylee@gmail.com>
2015-03-02 21:19:53 +0900

    030215 Bug Fixing

--- DreamCoast3D/DreamCoast3D/cUIMinimap.cpp
+++ DreamCoast3D/DreamCoast3D/cUIMinimap.cpp
@@ -38,18 +38,27 @@ void cUIMinimap::Setup(){
 
 void cUIMinimap::Start()
 {
-	//std::set<cGameObject*> setGameObject = m_pGameObjDelgate->GetObjectData();
-	//SetObject(setGameObject);
-	//cGameObject* pGameObject = m_pGameObjDelgate->GetPlayerData();
-	//D3DXVECTOR3 vec = pGameObject->GetPosition();
+	m_pGameObjDelgate->GetPlayerStatInfo();
+	std::set<cGameObject*> setGameObject = m_pGameObjDelgate->GetObjectData();
+	SetObject(setGameObject);
+	cGameObject* pGameObject = m_pGameObjDelgate->GetPlayerData();
+	D3DXVECTOR3 vec = m_pGameObjDelgate->GetPlayerData()->GetPosition();
+	SetPlayerPosition(&vec);
+	SetSight(30.f);
+
+	////cGameObject* pGameObject = 
+	//D3DXVECTOR3 vec = m_pGameObjDelgate->GetPlayerData()->GetPosition();
 	//SetPlayerPosition(&vec);
 	//SetSight(30.f);
-
 }
 
 void cUIMinimap::Update(float fDelta){
 	if (m_pUIRoot)
 		m_pUIRoot->Update(fDelta);
+
+	//float x = m_pGameObjDelgate->GetPlayerStatInfo()->fCurrentHp;
+	//D3DXVECTOR3 vec = m_pGameObjDelgate->GetPlayerData()->GetPosition();
+	//cGameObject* pGameObject = m_pGameObjDelgate->GetPlayerData();
 
 	//std::set<cGameObject*> setGameObject = m_pGameObjDelgate->GetObjectData();
 	//SetObject(setGameObject);
