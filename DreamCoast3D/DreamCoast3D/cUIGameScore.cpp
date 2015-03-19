6e408f2620f9610a7fb03ff473ceb9bbd813c9dc
Heedong Lee <arkinylee@gmail.com>
2015-03-19 16:03:07 +0900

    03192015 Score money icon

--- DreamCoast3D/DreamCoast3D/cUIGameScore.cpp
+++ DreamCoast3D/DreamCoast3D/cUIGameScore.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "cUIGameScore.h"
 #include "cUITextView.h"
+#include "cUIImageView.h"
 
 cUIGameScore::cUIGameScore()
 {
@@ -17,6 +18,14 @@ cUIGameScore::~cUIGameScore()
 
 void cUIGameScore::Setup(){
 	D3DXCreateSprite(g_pD3DDevice, &m_pSprite);
+	//moneyscoreIcon
+
+	cUIImageView* pImageView = new cUIImageView(m_pSprite);
+	pImageView->SetTextureFilename(std::string("../Resources/UI/UI_ShortCut/moneyscoreIcon.png"));
+	pImageView->SetScale(D3DXVECTOR3(1.0f, 1.0f, 1.0f));
+	pImageView->SetPosition(D3DXVECTOR3(0, 0, 0));
+	m_pUIRoot = pImageView;
+
 	cUITextView* pTextView = new cUITextView(m_pSprite);
 	std::stringstream ss;
 	if (m_pGameObjDelgate){
@@ -24,13 +33,15 @@ void cUIGameScore::Setup(){
 	}
 	pTextView->SetText(ss.str());
 	ss.str("");
-	pTextView->SetDrawTextFormat(DT_LEFT | DT_TOP | DT_WORDBREAK);
+	pTextView->SetDrawTextFormat(DT_CENTER | DT_VCENTER | DT_WORDBREAK);
 	pTextView->SetTextColor(D3DCOLOR_XRGB(255, 255, 255));
-	pTextView->SetPosition(10, 30);
-	ST_SIZE stSize = { 200, 200 };
+	pTextView->SetPosition(0, 0);
+
+	ST_SIZE stSize = m_pUIRoot->GetSize();
 	pTextView->SetSize(stSize);
-	m_pUIRoot = pTextView;
+	pImageView->AddChild(pTextView);
 	m_pText = pTextView;
+	SAFE_RELEASE(pTextView);
 }
 
 void cUIGameScore::Update(float fDelta){
